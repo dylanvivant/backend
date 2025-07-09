b27dbf4551250359710203ce550166c7c9e55760
Dylan V <153835867+dylanvivant@users.noreply.github.com>
2025-07-09 11:03:03 +0200

    commit

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.use(helmet());
 // CORS
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+    origin: process.env.FRONTEND_URL,
     credentials: true,
   })
 );
--- server.js
+++ server.js
@@ -50,7 +50,7 @@ app.use(helmet());
 // CORS
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+    origin: process.env.FRONTEND_URL,
     credentials: true,
   })
 );
